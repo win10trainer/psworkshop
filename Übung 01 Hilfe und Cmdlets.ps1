9741004d90feff583fa91618210d44b732d5593b
win10trainer <win10trainer@outlook.de>
2018-01-02 17:46:28 +0100

    test

---
+++ Übung 01 Hilfe und Cmdlets.ps1
@@ -0,0 +1,68 @@
+﻿#Testen Sie folgende Befehle:
+
+
+# Get-command zum Suchen von Befehlen
+# Wie können Sie Befehle, die Prozesse (engl. Process) betreffen, finden?
+
+
+
+# Finden Sie heraus, wie man get-command verwendet:
+
+
+
+# Suchen Sie mehrere Befehle gleichzeitig, die den Begriff ip oder address oder beide beinhalten.
+
+
+
+# Powershell stellt die Kommandos in Modulen bereit. Aus welchem Modul stammt get-netipaddress
+
+
+
+# Welche Befehle gibt es in dem Modul?
+
+
+
+# Finden Sie Befehle, mit denen man Powershell-Module verwalten kann.
+
+
+
+# Finden Sie heraus, was der Befehl get-modul bewirkt:
+
+
+
+# Gibt es einen Befehl, der alle Module auflistet?
+
+
+
+# Kann man Ereignisanzeige Systemlog anzeigen? Suchen Sie nach *event* Kommandos
+
+
+
+# Das gibt viele Ergebnisse.. Für Befehle die etwas Anzeigen, wird ein Verb Get benutzen: Get-*event*
+
+
+# Es passt get-eventlog. Welche Parameter hat der Befehl?
+
+
+
+# Die Hilfe gibt an, daß ein Logfilename verwendet werden kann und z.B. auch, daß 
+# man die ausgegebene Anzahl der Ereignisse auf die neuesten 10 beschränken kann.
+# Mit welchen Befehl können Sie die neuesten 10 Ereignisse des Systemlogfile anzeigen?
+
+
+
+# Die Hilfe umfasst auch die about_ Dateien. Mit welchem Befehl können Sie sie auflisten?
+
+
+
+# Wie erstellt man Aliase? Zeigen Sie die about Hilfe zum Thema Alias in einem eigenen Fenster an.
+
+
+
+# Erstellen Sie den Alias spro um einen Prozess zu stoppen.
+
+
+
+#Für welche CmdLet steht der alias gsv?
+
+
